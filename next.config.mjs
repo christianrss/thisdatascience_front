65ffe9e95c96f8df65f8fc2dfb9df99673339497
Christian <christianparadix@gmail.com>
2024-06-23 16:02:46 -0300

    api oficial

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/v1/:path*',
-                destination: 'http://localhost:8080/api/v1/:path*'
+                destination: 'https://api.thisdatascience.com/api/v1/:path*'
             }
         ]
     }
